2f62c84c5322e746b159c12ebab17b71e444861d
Phani Mohan. Y <phanimohan.yarlagadda@cognizant.com>
2015-08-17 17:53:44 +0530

    dev: Processing image using async.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Generating images from source json using GraphicsMagick
 
-GM APP
-
 
 ## Installation
 
@@ -14,3 +12,37 @@ GM APP
 * npm install or sudo npm install
 * node app.js
 
+## Configuration
+
+* app-config.json - Will consists of different types and quality of the image to generate. We can override the image quality per each type.
+```
+{
+  "types": {
+    "clips": {
+      "SD": {
+        "quality": "50",
+        "height": "500",
+        "width": "500"
+      },
+      "HD": {
+        "height": "800",
+        "width": "800"
+      }
+    }
+  },
+  "quality": {
+    "SD": "75",
+    "HD": "100"
+  }
+}
+```
+* source.json - Source file to read image, title & description to process
+```
+[
+  {
+    "title": "TheCarmichaelShow",
+    "description": "Description goes here... ",
+    "image": "http://www.nbc.com/sites/nbcunbc/files/files/styles/nbc_homepage_dynamic_slide_image/public/images/2015/8/11/2015-0812-FoodFighters-1920x1080-TVE-SB.jpg?itok=1VaoCu7y"
+  }
+]
+```
--- app.js
+++ app.js
@@ -10,61 +10,91 @@ var fontFile = 'fonts/SourceSansPro/TTF/SourceSansPro-Semibold.ttf';
 var titleColor = '#FFFFFF';
 var descColor = '#CCCCCC';
 
-_.each(config['types'], function(element, key, array){
+var processImage = function (imgResConfig, resType, sElement, sKey, resDir, callback) {
+	var imgName = path.parse(sElement['image']).name;
+	var ext = path.extname(sElement['image']).split('?');
+
+	var imgQuality = (imgResConfig['quality']) ? imgResConfig['quality'] : config['quality'][resType];
+
+	var imgToSave = resDir + '/' + imgName + ext['0'];
+	var boxYPosition = 65;
+
+    gm(sElement['image'])
+		.resize(imgResConfig['width'], imgResConfig['height'])
+		.autoOrient()
+		.quality(imgQuality)
+		.write(imgToSave, function (err) {
+		  	if (err) console.log( 'error: ' + err);
+		  	if (!err){
+          		// obtain the size of an image to append text.
+				gm(imgToSave)
+				.size(function (err, size) {
+				  	if (!err) {
+				    	boxYPosition = size.height - boxYPosition;
+
+				    	// Append text to generated image.
+				    	gm(imgToSave)
+				    		.quality(imgQuality)
+							.matte()
+							.fill('rgba(0, 0, 0, 65)')
+							.drawRectangle(0, boxYPosition, imgResConfig['width'], imgResConfig['height'])
+							.font(fontFile)
+							.gravity('SouthWest')
+							.fill(titleColor)
+							.fontSize(16)
+							.drawText(10, 45, sElement['title'])
+							.fill(descColor)
+							.fontSize(14)
+							.drawText(10, 25, sElement['description'])
+							.write(imgToSave, function (err) {
+							  if (err) console.log( 'error: ' + err);
+							  callback();
+							});
+				  	}
+				});
+          	}
+		});
+};
+
+var processResolution = function (imgResConfig, resType, typeDir, callback) {
+	var resDir = typeDir + '/' + resType;
+	mkdirp(resDir, function (err) {
+	    if (err) console.error(err)
+	});
+
+	async.forEachOf(source, 
+		function(sElement, sKey, callback) {
+			processImage(imgResConfig, resType, sElement, sKey, resDir, callback);
+		}, 
+		function(err){
+	      callback();
+	});
+};
+
+var processConfigItem = function (type, key, callback) {
 	var typeDir = filesDir + key;
 	mkdirp(typeDir, function (err) {
 	    if (err) console.error(err)
 	});
 
-	_.each(element, function(e, k, a){
-		var resDir = typeDir + '/' + k;
-		mkdirp(resDir, function (err) {
-		    if (err) console.error(err)
-		});
-
-	    _.each(source, function(sElement, sKey, sArray){
-	    	var imgName = path.parse(sElement['image']).name;
-	    	var ext = path.extname(sElement['image']).split('?');
-
-	    	var imgToSave = resDir + '/' + imgName + ext['0'];
-	    	var boxYPosition = 65;
-
-		    gm(sElement['image'])
-				.resize(e['width'], e['height'])
-				.autoOrient()
-				.quality(100)
-				.write(imgToSave, function (err) {
-				  	if (err) console.log( 'error: ' + err);
-				  	if (!err){
-		          		// obtain the size of an image to append text.
-						gm(imgToSave)
-						.size(function (err, size) {
-						  	if (!err) {
-						    	boxYPosition = size.height - boxYPosition;
-
-						    	// Append text to generated image.
-						    	gm(imgToSave)
-						    		.quality(100)
-									.matte()
-									.fill('rgba(0, 0, 0, 65)')
-									.drawRectangle(0, boxYPosition, e['width'], e['height'])
-									.font(fontFile)
-									.gravity('SouthWest')
-									.fill(titleColor)
-									.fontSize(16)
-									.drawText(10, 45, sElement['title'])
-									.fill(descColor)
-									.fontSize(14)
-									.drawText(10, 25, sElement['description'])
-									.write(imgToSave, function (err) {
-									  if (err) console.log( 'error: ' + err);
-									});
-						  	}
-						});
-		          	}
-				});
-	    });
+	async.forEachOf(type, 
+		function(imgResConfig, resType, callback) {
+			processResolution(imgResConfig, resType, typeDir, callback);
+		}, 
+		function(err){
+	      console.log('Images generated for ' + key);
+	      callback();
 	});
+};
+
+async.forEachOf(config['types'], 
+	function(type, key, callback) {
+		processConfigItem(type, key, callback);
+	}, 
+	function(err){
+      doSomethingOnceAllAreDone();
 });
 
-console.log('Completed--->');
+function doSomethingOnceAllAreDone() {
+    console.log("Everything is done.");
+}
